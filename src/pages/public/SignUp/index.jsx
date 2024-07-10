3eb3feecf918e1ef410c7351595e275c7a1e5986
Darkmetak <matporto03@gmail.com>
2024-07-10 14:17:43 -0300

    styles: Change signup styles

--- src/pages/public/SignUp/index.jsx
+++ src/pages/public/SignUp/index.jsx
@@ -1,9 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useNavigate } from 'react-router-dom'
 import { useForm, Controller } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import z from 'zod'
 import dayjs from 'dayjs'
+
+import graphImage from '@assets/grafo.png'
 
 import { api } from '@lib/api'
 import { useToast } from '@hooks/useToast'
@@ -13,7 +14,7 @@ import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 import styles from './styles.module.css'
 
-const schema = z
+const signUpSchema = z
   .object({
     nome: z
       .string({ required_error: 'Esse campo é obrigatório' })
@@ -94,15 +95,15 @@ export function SignUp() {
     control,
     formState: { errors, isSubmitting },
   } = useForm({
-    resolver: zodResolver(schema),
+    resolver: zodResolver(signUpSchema),
   })
 
   const navigate = useNavigate()
   const { showToast, ToastComponents } = useToast()
 
-  async function handleCadastrarUsuario({ _confirmarSenha, ...rest }) {
+  async function handleRegisterUser(data) {
     try {
-      await api.post('/clientes', { ...rest })
+      await api.post('/clientes', { ...data })
       navigate('/login')
     } catch (error) {
       const isAppError = error instanceof AppError
@@ -116,186 +117,207 @@ export function SignUp() {
   }
 
   return (
-    <main className={styles.container}>
-      <h1>Abrir conta</h1>
-      <form
-        className={styles.form}
-        onSubmit={handleSubmit(handleCadastrarUsuario)}
-      >
-        <div>
-          <Controller
-            name="nome"
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                label="Nome Completo"
-                type="text"
-                onChange={onChange}
-                onBlur={onBlur}
-                value={value}
-                errors={errors.nome?.message}
-              />
-            )}
-            control={control}
-          />
-          <Controller
-            name="dataNascimento"
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                label="Data de Nascimento"
-                type="date"
-                onChange={onChange}
-                onBlur={onBlur}
-                value={value}
-                errors={errors.dataNascimento?.message}
-              />
-            )}
-            control={control}
-          />
-          <Controller
-            name="rg"
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                label="RG"
-                type="text"
-                onChange={onChange}
-                onBlur={onBlur}
-                value={value}
-                errors={errors.rg?.message}
-                placeholder="12.345.678-9"
-                mask="99.999.999-*"
-              />
-            )}
-            control={control}
-          />
-          <Controller
-            name="cpf"
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                label="CPF"
-                type="text"
-                onChange={onChange}
-                onBlur={onBlur}
-                value={value}
-                errors={errors.cpf?.message}
-                placeholder="123.456.789-10"
-                mask="999.999.999-99"
-              />
-            )}
-            control={control}
-          />
-          <Controller
-            name="cep"
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                label="CEP"
-                type="text"
-                onChange={onChange}
-                onBlur={onBlur}
-                value={value}
-                errors={errors.cep?.message}
-                placeholder="01234-567"
-                mask="99999-999"
-              />
-            )}
-            control={control}
-          />
-          <Controller
-            name="endereco"
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                label="Endereço"
-                type="text"
-                onChange={onChange}
-                onBlur={onBlur}
-                value={value}
-                errors={errors.endereco?.message}
-              />
-            )}
-            control={control}
-          />
-          <Controller
-            name="numero"
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                label="Numero"
-                type="text"
-                onChange={onChange}
-                onBlur={onBlur}
-                value={value}
-                errors={errors.numero?.message}
-              />
-            )}
-            control={control}
-          />
+    <main className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.information}>
+          <h1>
+            Preencha todos os campos para pedir sua conta e cartão de crédito
+          </h1>
+
+          <div>
+            <img src={graphImage} alt="" />
+          </div>
         </div>
-        <div>
-          <Controller
-            name="telefone"
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                label="Telefone"
-                type="text"
-                onChange={onChange}
-                onBlur={onBlur}
-                value={value}
-                errors={errors.telefone?.message}
-                placeholder="+55 11 99999-9999"
-                mask="+5\5 99 99999-9999"
-              />
-            )}
-            control={control}
-          />
-          <Controller
-            name="email"
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                label="E-mail"
-                type="email"
-                onChange={onChange}
-                onBlur={onBlur}
-                value={value}
-                errors={errors.email?.message}
-              />
-            )}
-            control={control}
-          />
-          <Controller
-            name="senha"
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                label="Senha"
-                type="password"
-                onChange={onChange}
-                onBlur={onBlur}
-                value={value}
-                errors={errors.senha?.message}
-              />
-            )}
-            control={control}
-          />
-          <Controller
-            name="confirmarSenha"
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                label="Confirmar Senha"
-                type="password"
-                onChange={onChange}
-                onBlur={onBlur}
-                value={value}
-                errors={errors.confirmarSenha?.message}
-              />
-            )}
-            control={control}
-          />
-          <Button
-            titulo="Concluir"
-            tipo="primario"
-            type="submit"
-            style={{ maxWidth: '20rem', alignSelf: 'center', width: '100%' }}
-            disabled={isSubmitting}
-          />
-        </div>
-      </form>
+
+        <form
+          className={styles.form}
+          onSubmit={handleSubmit(handleRegisterUser)}
+        >
+          <fieldset>
+            <legend>Dados Pessoais</legend>
+
+            <Controller
+              name="nome"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  label="Nome Completo"
+                  type="text"
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  errors={errors.nome?.message}
+                />
+              )}
+              control={control}
+            />
+
+            <Controller
+              name="cpf"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  label="CPF"
+                  type="text"
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  errors={errors.cpf?.message}
+                  placeholder="123.456.789-10"
+                  mask="999.999.999-99"
+                />
+              )}
+              control={control}
+            />
+
+            <Controller
+              name="rg"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  label="RG"
+                  type="text"
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  errors={errors.rg?.message}
+                  placeholder="12.345.678-9"
+                  mask="99.999.999-*"
+                />
+              )}
+              control={control}
+            />
+
+            <Controller
+              name="dataNascimento"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  label="Data de Nascimento"
+                  type="date"
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  errors={errors.dataNascimento?.message}
+                />
+              )}
+              control={control}
+            />
+
+            <Controller
+              name="telefone"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  label="Telefone"
+                  type="text"
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  errors={errors.telefone?.message}
+                  placeholder="+55 11 99999-9999"
+                  mask="+5\5 99 99999-9999"
+                />
+              )}
+              control={control}
+            />
+            <Controller
+              name="email"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  label="E-mail"
+                  type="email"
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  errors={errors.email?.message}
+                />
+              )}
+              control={control}
+            />
+          </fieldset>
+
+          <fieldset>
+            <legend>Endereço</legend>
+
+            <Controller
+              name="cep"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  label="CEP"
+                  type="text"
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  errors={errors.cep?.message}
+                  placeholder="01234-567"
+                  mask="99999-999"
+                />
+              )}
+              control={control}
+            />
+            <Controller
+              name="endereco"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  label="Endereço"
+                  type="text"
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  errors={errors.endereco?.message}
+                />
+              )}
+              control={control}
+            />
+            <Controller
+              name="numero"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  label="Numero"
+                  type="text"
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  errors={errors.numero?.message}
+                />
+              )}
+              control={control}
+            />
+          </fieldset>
+
+          <fieldset>
+            <legend>Conta</legend>
+
+            <Controller
+              name="senha"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  label="Senha"
+                  type="password"
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  errors={errors.senha?.message}
+                />
+              )}
+              control={control}
+            />
+            <Controller
+              name="confirmarSenha"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  label="Confirmar Senha"
+                  type="password"
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  errors={errors.confirmarSenha?.message}
+                />
+              )}
+              control={control}
+            />
+          </fieldset>
+
+          <Button title="Criar Conta" type="submit" disabled={isSubmitting} />
+        </form>
+      </div>
+
       {ToastComponents}
     </main>
   )
